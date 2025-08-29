7fc1c8dd7793f241c06bb07d82835587b8b8d100
dorincreciun <creciundorin25@gmail.com>
2025-08-29 12:26:43 +0300

    Initialize project structure with React, TypeScript, and Vite; add TailwindCSS setup, ESLint configuration, and initial dependencies.

---
+++ src/app/main.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import './styles/index.css'
+
+const Root = () => {
+    return <></>
+}
+
+createRoot(document.getElementById('root')!).render(<Root />)
