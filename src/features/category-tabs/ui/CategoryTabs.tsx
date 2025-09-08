dec5a6fe7be3ea7034395cbf113725adbdc65cb9
Creciun <creciundorin25@gmail.com>
2025-09-08 22:10:04 +0300

    fix(ui/tab): small adjusted color background

--- src/features/category-tabs/ui/CategoryTabs.tsx
+++ src/features/category-tabs/ui/CategoryTabs.tsx
@@ -1,7 +1,7 @@
 export const CategoryTabs = () => {
     return (
         <div>
-            <div className={"bg-[#FAFAFA] py-1.5 px-3 rounded-xl"}>
+            <div className={"bg-[#F9FAFB] py-1.5 px-3 rounded-xl"}>
                 <button className={'py-2.5 px-4 font-bold text-base text-[#202020]'}>Мясные</button>
                 <button className={'py-2.5 px-4 font-bold text-base bg-white rounded-2xl shadow-[0_4px_4px_rgba(0,0,0,0.05)]'}>Острые</button>
             </div>
