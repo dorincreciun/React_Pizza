bdce5c8ad84f3be59565535f8b69081dff50bdd7
dorincreciun <creciundorin25@gmail.com>
2025-09-10 16:06:57 +0300

    feat(features/product): added SegmentedControl for select-dough, select-size

--- src/features/product/select-dough/ui/index.tsx
+++ src/features/product/select-dough/ui/index.tsx
@@ -1,11 +1,7 @@
+import { SegmentedControl } from "../../../../shared/ui/SegmentedControl";
+
+const dough = ["Традиционное", "Тонкое"];
+
 export const SelectDough = () => {
-	return (
-		<div className={"rounded-[30px] bg-[#ECECEC] p-[2px]"}>
-			<button className={"rounded-[30px] bg-white px-10 py-2"}>
-				Маленькая
-			</button>
-			<button className={"rounded-[30px] px-10 py-2"}>Маленькая</button>
-			<button className={"rounded-[30px] px-10 py-2"}>Маленькая</button>
-		</div>
-	);
+	return <SegmentedControl properties={dough} />;
 };
--- src/features/product/select-size/ui/index.tsx
+++ src/features/product/select-size/ui/index.tsx
@@ -1,30 +1,7 @@
-import { useState } from "react";
+import { SegmentedControl } from "../../../../shared/ui/SegmentedControl";
 
 const sizes = ["Маленькая", "Средняя", "Большая"];
 
 export const SizeSelector = () => {
-	const [activeIndex, setActiveIndex] = useState(0);
-
-	return (
-		<div className="relative flex rounded-[30px] bg-[#ECECEC] p-[2px]">
-			{/* Indicator glisant */}
-			<div
-				className="absolute top-[2px] left-[2px] h-[calc(100%-4px)] w-1/3 rounded-[30px] bg-white transition-transform duration-300 ease-in-out"
-				style={{
-					transform: `translateX(${activeIndex * 100}%)`,
-				}}
-			/>
-
-			{/* Butoane */}
-			{sizes.map((label, index) => (
-				<button
-					key={label}
-					onClick={() => setActiveIndex(index)}
-					className="relative z-10 flex-1 rounded-[30px] px-10 py-2 text-center"
-				>
-					{label}
-				</button>
-			))}
-		</div>
-	);
+	return <SegmentedControl properties={sizes} />;
 };
