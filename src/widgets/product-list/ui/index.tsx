4ddb6b73ef7868440c2ecb6a8a3ab59501b682c7
Creciun <creciundorin25@gmail.com>
2025-09-15 20:48:04 +0300

    refactor(ui/product-list): extract tabs logic to `ProductList` and simplify `ProductsPage` structure

--- src/widgets/product-list/ui/index.tsx
+++ src/widgets/product-list/ui/index.tsx
@@ -1,3 +1,70 @@
+import { Tabs } from "@/shared/ui/Tabs";
+import { Filter } from "@/features/product/filter";
+
+const tabLists = [
+	{
+		label: "Все",
+		value: "Все",
+	},
+	{
+		label: "Мясные",
+		value: "Мясные",
+	},
+	{
+		label: "Острые",
+		value: "Острые",
+	},
+	{
+		label: "Сладкие",
+		value: "Сладкие",
+	},
+	{
+		label: "Вегетарианские",
+		value: "Вегетарианские",
+	},
+	{
+		label: "С курицей",
+		value: "С курицей",
+	},
+];
+
 export const ProductList = () => {
-	return <div className={""}></div>;
+	return (
+		<div className={"pt-10"}>
+			<Tabs defaultValue="descriere">
+				<div className={"pb-9"}>
+					<h1 className={"pb-9 text-4xl font-extrabold"}>
+						Все пиццы
+					</h1>
+					<div className={"flex items-center justify-between gap-10"}>
+						<Tabs.List>
+							{tabLists.map((item) => (
+								<Tabs.Trigger
+									key={item.value}
+									value={item.value}
+								>
+									{item.value}
+								</Tabs.Trigger>
+							))}
+						</Tabs.List>
+					</div>
+				</div>
+
+				<div className="flex gap-10">
+					<Filter />
+					<div className={"flex-auto bg-gray-50"}>
+						<Tabs.Content value="Все">
+							Conținutul pentru tab-ul Descriere.
+						</Tabs.Content>
+						<Tabs.Content value="Мясные">
+							Conținutul pentru tab-ul Ingrediente.
+						</Tabs.Content>
+						<Tabs.Content value="Острые">
+							Conținutul pentru tab-ul Recenzii.
+						</Tabs.Content>
+					</div>
+				</div>
+			</Tabs>
+		</div>
+	);
 };
