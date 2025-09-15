4e73839cb12afa696aee1396e847c3ab5ce5739e
dorincreciun <creciundorin25@gmail.com>
2025-09-15 10:49:09 +0300

    feat(features/filter): first structure for filter

--- src/widgets/product-list/ui/index.tsx
+++ src/widgets/product-list/ui/index.tsx
@@ -1,25 +1,40 @@
 import { Checkbox } from "@/shared/ui/Checkbox";
 import { Radio } from "@/shared/ui/Radio";
+import { Button } from "@/shared/ui/Button";
 
 export const ProductList = () => {
 	return (
 		<div>
-			<div className={"flex flex-col gap-4"}>
-				<Checkbox label={"Можно собирать"} />
-				<Checkbox label={"Новинки"} />
-			</div>
+			<div className={"max-w-[224px]"}>
+				<div className={"my-9 text-[22px] font-bold"}>Фильтрация</div>
 
-			<Radio name={"test"} label={"Test 2"} />
-			<Radio name={"test"} label={"Test 3"} />
+				<div className={"flex flex-col gap-4"}>
+					<Checkbox label={"Можно собирать"} />
+					<Checkbox label={"Новинки"} />
+				</div>
 
-			<div className={"flex flex-col gap-4"}>
-				<Checkbox label={"Сырный соус"} />
-				<Checkbox label={"Моцарелла"} />
-				<Checkbox label={"Чеснок"} />
-				<Checkbox label={"Солённые огурчики"} />
-				<Checkbox label={"Красный лук"} />
-				<Checkbox label={"Красный лук"} />
-				<Checkbox label={"Томаты"} />
+				<div
+					className={
+						"my-6 flex flex-col gap-4 border-t border-b border-[#F6F6F6] py-6"
+					}
+				>
+					<Radio name={"test"} label={"Test 2"} />
+					<Radio name={"test"} label={"Test 3"} />
+				</div>
+
+				<div className={"flex flex-col gap-4"}>
+					<Checkbox label={"Сырный соус"} />
+					<Checkbox label={"Моцарелла"} />
+					<Checkbox label={"Чеснок"} />
+					<Checkbox label={"Солённые огурчики"} />
+					<Checkbox label={"Красный лук"} />
+					<Checkbox label={"Красный лук"} />
+					<Checkbox label={"Томаты"} />
+				</div>
+
+				<Button size={"large"} className={"mt-10 w-full"}>
+					Применить
+				</Button>
 			</div>
 		</div>
 	);
